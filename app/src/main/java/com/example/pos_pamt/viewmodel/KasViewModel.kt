a56561ee03c05925e3ee76da7b3dbf711b379888
Florahpn <florahapsari@student.ub.ac.id>
2026-05-13 11:22:34 +0700

    tambah data, repository, viewmodel

---
+++ app/src/main/java/com/example/pos_pamt/viewmodel/KasViewModel.kt
@@ -0,0 +1,36 @@
+package com.pos.pamt.viewmodel
+
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.pos.pamt.data.Kas
+import com.pos.pamt.repository.KasRepository
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.launch
+
+class KasViewModel : ViewModel() {
+
+    private val repository = KasRepository()
+    private val _kasState = MutableStateFlow<DataUiState<List<Kas>>>(DataUiState.Idle)
+    val kasState: StateFlow<DataUiState<List<Kas>>> = _kasState
+
+    init {
+        loadKas()
+    }
+
+    fun loadKas() {
+        viewModelScope.launch {
+            try {
+                _kasState.value = DataUiState.Loading
+
+                val result = repository.getSemuaKas()
+                _kasState.value = DataUiState.Success(result)
+
+            } catch (e: Exception) {
+                _kasState.value = DataUiState.Error(
+                    message = e.message ?: "Gagal memuat data kas."
+                )
+            }
+        }
+    }
+}
