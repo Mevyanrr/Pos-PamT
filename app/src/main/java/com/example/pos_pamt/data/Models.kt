9ca180c025171ffe7ca0860ad4e17ff100975f61
Syumaylah <syumaylah1@gmail.com>
2026-05-19 09:42:01 +0700

    tambah Models.kt Pelanggan

--- app/src/main/java/com/example/pos_pamt/data/Models.kt
+++ app/src/main/java/com/example/pos_pamt/data/Models.kt
@@ -5,20 +5,34 @@ import kotlinx.serialization.Serializable
 
 @Serializable
 data class Barang(
-    val id      : String  = "",
-    val nama    : String  = "",
-    val harga   : Double  = 0.0,
-    val stok    : Double  = 0.0,
+    val id : String  = "",
+    val nama : String  = "",
+    val harga : Double  = 0.0,
+    val stok : Double  = 0.0,
     @SerialName("is_active")
     val isActive: Boolean = true
 )
 
 @Serializable
 data class Kas(
-    val id      : String  = "",
+    val id : String  = "",
     @SerialName("nama_kas")
-    val nama    : String  = "",
-    val saldo   : Double  = 0.0,
+    val nama : String  = "",
+    val saldo : Double  = 0.0,
     @SerialName("is_active")
     val isActive: Boolean = true
+)
+
+@Serializable
+data class Pelanggan(
+    val id: String = "",
+    val nama: String = "",
+    @SerialName("no_telp")
+    val noTelp: String = "",
+    @SerialName("is_active")
+    val isActive: Boolean = true,
+    @SerialName("created_at")
+    val createdAt: String = "",
+    @SerialName("updated_at")
+    val updatedAt: String = ""
 )
